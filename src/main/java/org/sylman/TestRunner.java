058c59f911745cfda909e1527080e0d7ed973c2b
Lyubov <l.musilmanbekova12@gmail.com>
2024-04-01 23:15:59 +0300

    Added: TestRunner and Test

--- src/main/java/org/sylman/TestRunner.java
+++ src/main/java/org/sylman/TestRunner.java
@@ -1,2 +1,73 @@
-package org.sylman;public class TestRunner {
+package org.sylman;
+
+import org.sylman.myAnnotations.*;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestRunner {
+    public static void run(Class<?> testClass) {
+        final Object testObj = initTestObj(testClass);
+        List<Method> testMethods = new ArrayList<>();
+        List<Method> beforeAllMethods = new ArrayList<>();
+        List<Method> beforeEachMethods = new ArrayList<>();
+        List<Method> afterEachMethods = new ArrayList<>();
+        List<Method> afterAllMethods = new ArrayList<>();
+
+        for (Method method : testClass.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(Test.class)) {
+                testMethods.add(method);
+            }
+            if (method.isAnnotationPresent(BeforeAll.class)) {
+                beforeAllMethods.add(method);
+            }
+            if (method.isAnnotationPresent(BeforeEach.class)) {
+                beforeEachMethods.add(method);
+            }
+            if (method.isAnnotationPresent(AfterEach.class)) {
+                afterEachMethods.add(method);
+            }
+            if (method.isAnnotationPresent(AfterAll.class)) {
+                afterAllMethods.add(method);
+            }
+        }
+
+        executeMethods(beforeAllMethods, testObj);
+
+        for (Method testMethod : testMethods) {
+            executeMethods(beforeEachMethods, testObj);
+            try {
+                testMethod.invoke(testObj);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+            executeMethods(afterEachMethods, testObj);
+        }
+        executeMethods(afterAllMethods, testObj);
+    }
+
+    private static void executeMethods(List<Method> methods, Object object) {
+        Collections.reverse(methods);
+        for (Method method : methods) {
+            try {
+                method.invoke(object);
+            } catch (IllegalAccessException | InvocationTargetException e){
+            throw new RuntimeException();
+        }}
+    }
+
+    private static Object initTestObj(Class<?> testClass) {
+        try {
+            Constructor<?> noArgsConstruction = testClass.getConstructor();
+            return noArgsConstruction.newInstance();
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("Don't have constructor by default");
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("Filed to create test class object");
+        }
+    }
 }
--- src/main/java/org/sylman/TestRunnerDemo.java
+++ src/main/java/org/sylman/TestRunnerDemo.java
@@ -1,2 +1,45 @@
-package org.sylman;public class TestRunnerDemo {
+package org.sylman;
+
+import org.sylman.myAnnotations.*;
+
+public class TestRunnerDemo {
+    public static void main(String[] args) {
+        TestRunner.run(TestRunnerDemo.class);
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before all tests");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("Before each test");
+    }
+
+    @Test
+    @Order(1)
+    public void test1() {
+        System.out.println("First test");
+        Asserter.assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    @Order(2)
+    public void test2() {
+        System.out.println("Second test");
+        Asserter.assertEquals(4, 2 * 2);
+    }
+
+    @AfterEach
+    public static void afterEach() {
+        System.out.println("After each test");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After all tests");
+    }
 }
+
+
