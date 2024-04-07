75506020284d723f5c4b01c1bbfb8baea18c2496
Lyubov <l.musilmanbekova12@gmail.com>
2024-04-07 22:36:53 +0300

    Modified: pom.xml

---
+++ src/main/java/org/sylman/seminar4/Student.java
@@ -0,0 +1,47 @@
+package org.sylman.seminar4;
+
+public class Students {
+    private int id;
+    private String firstName;
+    private String secondName;
+    private int age;
+
+    public Students(int id, String firstName, String secondName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/org/sylman/seminar4/StudentDAO.java
@@ -0,0 +1,2 @@
+package org.sylman.seminar4;public class StudentDAO {
+}
